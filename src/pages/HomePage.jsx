0348e64a27fe385f33d13f279acbdd57e01d71df
Gokul Chinna <gokulchinna110@gmail.com>
2025-12-15 12:48:24 +0530

    Fetch data from backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,14 @@ import { Header } from '../components/Header';
 import {products} from '../../StartingCode/data/products';
 
 export function HomePage() {
+
+    fetch('http://localhost:3000/api/products')
+        .then((response) => {
+            return response.json();
+        }).then((data) => {
+            console.log(data);
+        });
+
     return (
         <>
             <link rel="icon" href="images/home-favicon.png" />
