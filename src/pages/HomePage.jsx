a178b803a69d58c43db035a2ca2f9dfbda6fa675
Gokul Chinna <gokulchinna110@gmail.com>
2025-12-15 13:34:30 +0530

    use data from backend to generate html

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,21 @@
 import axios from 'axios';
+import { useEffect,useState } from 'react';
 import './HomePage.css';
 import CheckMark from '../assets/images/icons/checkmark.png';
 
 import { Header } from '../components/Header';
-import {products} from '../../StartingCode/data/products';
+
 
 export function HomePage() {
 
-    axios.get('http://localhost:3000/api/products')
-        .then((response) => {
-            console.log(response.data);
-        });
+    const [products,setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/api/products')
+            .then((response) => {
+                setProducts(response.data);
+            });
+    },[]);
 
     return (
         <>
